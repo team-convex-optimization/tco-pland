f724f041ce0d66e4d0914334c555057ffde91716
Eduardo Lira <edulira@me.com>
2021-03-12 19:29:34 +0100

    Taking points from track center

--- code/trajection.c
+++ code/trajection.c
@@ -13,6 +13,8 @@ void show_target_lines(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
 
 void plot_square(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t pointx, uint16_t pointy, int size, uint8_t color)
 {
+    if (pointx < 0 || pointy < 0)
+        return;
     const int radius = size/2;
     for (int i = size-radius; i < size+radius; i++)
     {
@@ -29,57 +31,54 @@ void plot_square(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t poin
 }
 
 
-void plot_targets(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
+uint8_t plot_targets(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
 {
     const uint16_t target_line_0 = (TCO_SIM_HEIGHT) / 5;
     const uint16_t target_line_1 = (TCO_SIM_HEIGHT) / 3;
     const uint16_t center_width = TCO_SIM_WIDTH /2;
-    const uint8_t segmented_line_width = 5;
 
-    uint16_t top_line_l, top_line_r, bot_line_l, bot_line_r = 0;
-    uint8_t switch_lines = 0; /* Bool */
+    uint16_t top_line_l, top_line_r, bot_line_r, bot_line_l = 0;
+
 
     /* Find edges for target_line_0 */
-    for (int i = 0; i < TCO_SIM_WIDTH; i++)
+    for (uint16_t i = center_width; i < TCO_SIM_WIDTH; i++)
     {
         if ((*pixels)[target_line_0][i] == 255)
         {
-            if (switch_lines == 0)
-            {
-                switch_lines++;
-                top_line_l = i;
-                i += segmented_line_width;
-            } 
-            else 
-            {
-                top_line_r = i;
-            }
-        }
+            top_line_r = i;
+            break;
+        } 
+    }
+    for (uint16_t i = center_width; i > 0; i--)
+    {
+        if ((*pixels)[target_line_0][i] == 255)
+        {
+            top_line_l = i;
+            break;
+        } 
     }
 
-    switch_lines = 0;
     /* Find edges for target_line_1 */
-    for (int i = 0; i < TCO_SIM_WIDTH; i++)
+    for (uint16_t i = center_width; i < TCO_SIM_WIDTH; i++)
     {
         if ((*pixels)[target_line_1][i] == 255)
         {
-            if (switch_lines == 0)
-            {
-                switch_lines++;
-                bot_line_l = i;
-                i += segmented_line_width;
-            } 
-            else 
-            {
-                bot_line_r = i;
-            }
-        }
+            bot_line_r = i;
+            break;
+        } 
     }
-
+    for (uint16_t i = center_width; i > 0; i--)
+    {
+        if ((*pixels)[target_line_1][i] == 255)
+        {
+            bot_line_l = i;
+            break;
+        } 
+    }
+    
     /* Paint the edges gray */
     plot_square(pixels, top_line_l, target_line_0, 10, 128);
     plot_square(pixels, top_line_r, target_line_0, 10, 192);
-
     plot_square(pixels, bot_line_l, target_line_1, 10, 128);
     plot_square(pixels, bot_line_r, target_line_1, 10, 192);
 }
