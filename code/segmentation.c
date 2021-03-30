738c6c443517d5c61e0dfb7d88770cd84d856627
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-03-30 12:35:21 +0200

    Remove flood fill for now

--- code/segmentation.c
+++ code/segmentation.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "segmentation.h"
+#include "tco_libd.h"
 
 void segment(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
 {
@@ -17,52 +18,16 @@ void segment(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
             }
 
             else if (height_idx + look_ahead_length < TCO_SIM_HEIGHT &&
-                abs((*pixels)[height_idx][width_idx] - (*pixels)[height_idx + look_ahead_length][width_idx]) > delta_threshold)
+                     abs((*pixels)[height_idx][width_idx] - (*pixels)[height_idx + look_ahead_length][width_idx]) > delta_threshold)
             {
                 (*pixels)[height_idx][width_idx] = 255;
                 continue;
             }
 
-            else 
+            else
             {
                 (*pixels)[height_idx][width_idx] = 0;
             }
         }
     }
 }
-
-/* The below is used for flood_filling the image. */
-
-/*
-uint8_t *get_pixel(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t x, uint16_t y) {
-    if (y < 0 || y > TCO_SIM_HEIGHT || x < 0 || x > TCO_SIM_WIDTH)
-        return NULL;
-    return pixels[y][x];
-}
-
-uint8_t get_pixel_color(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t x, uint16_t y) {
-    if (y < 0 || y > TCO_SIM_HEIGHT || x < 0 || x > TCO_SIM_WIDTH)
-        return NULL; //error
-    return (*pixels)[y][x];
-}
-
-void set_pixel(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t x, uint16_t y, uint8_t color) {
-    (*pixels)[y][x] = color;
-}
-
-void flood_fill(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t x, uint16_t y) {
-    if (y < 0 || y >= TCO_SIM_HEIGHT || x < 0 || x >= TCO_SIM_WIDTH)
-    {
-        return;
-    }
-    
-    if(get_pixel_color(pixels, x, y) == 0)
-	{
-        set_pixel(pixels, x, y, 255);
-		flood_fill(pixels, x+1, y); 
-		flood_fill(pixels, x, y+1);
-		flood_fill(pixels, x-1, y);
-		flood_fill(pixels, x, y-1);
-	}
-}
-*/
