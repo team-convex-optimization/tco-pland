197aeeadbc5b3b899f5c8b1f4031f4dbf6d82d45
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-04-01 13:04:05 +0200

    Update shmem

--- code/vector.c
+++ code/vector.c
@@ -5,18 +5,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-void find_edges_scan(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t center_width, uint16_t height, uint16_t (*edges)[2])
+void find_edges_scan(uint8_t (*pixels)[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH], uint16_t center_width, uint16_t height, uint16_t (*edges)[2])
 {
-    for (uint16_t i = center_width; i < TCO_SIM_WIDTH - SEGMENTATION_DEADZONE; i++)
+    for (uint16_t i = center_width; i < TCO_FRAME_WIDTH - SEGMENTATION_DEADZONE; i++)
     {
         if ((*pixels)[height][i] == 255)
         {
-            (*edges)[0] = i; 
+            (*edges)[0] = i;
             goto right_edge; /* If the edge was found, do not set to err value */
-        } 
+        }
     }
-    (*edges)[0] = ERR_POINT; 
-    right_edge:
+    (*edges)[0] = ERR_POINT;
+right_edge:
 
     for (uint16_t i = center_width; i > 0 + SEGMENTATION_DEADZONE; i--)
     {
@@ -24,43 +24,44 @@ void find_edges_scan(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], uint16_t 
         {
             (*edges)[1] = i;
             return;
-        } 
+        }
     }
 
-    (*edges)[1] = ERR_POINT; 
+    (*edges)[1] = ERR_POINT;
 }
 
-void plot_vector_points(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
+void plot_vector_points(uint8_t (*pixels)[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH])
 {
     /* Take scanline for firstrow at bottom. Then go up an increment and spread from center of last 2 points, looking for next point.
         Do this and there should be a series of points that are on the line! */
-    const uint16_t center_width = TCO_SIM_WIDTH / 2;
+    const uint16_t center_width = TCO_FRAME_WIDTH / 2;
 
-    const uint16_t target_lines[NUM_VECTOR_POINTS] = { //TODO : Dynamically initialize me.
-        (28 * TCO_SIM_HEIGHT)  / 72,  /* Closest to the car */
-        (27 * TCO_SIM_HEIGHT)  / 72,
-        (26 * TCO_SIM_HEIGHT)  / 72,
-        (25 * TCO_SIM_HEIGHT)  / 72,
-        (24 * TCO_SIM_HEIGHT)  / 72,
-        (23 * TCO_SIM_HEIGHT)  / 72,
-        (22 * TCO_SIM_HEIGHT)  / 72,
-        (21 * TCO_SIM_HEIGHT)  / 72,
-        (20 * TCO_SIM_HEIGHT)  / 72,
-        (19 * TCO_SIM_HEIGHT)  / 72,
-        (18 * TCO_SIM_HEIGHT)  / 72,
-        (17 * TCO_SIM_HEIGHT)  / 72,
-        (16 * TCO_SIM_HEIGHT)  / 72,  
-        (15 * TCO_SIM_HEIGHT)  / 72,
-        (14 * TCO_SIM_HEIGHT)  / 72,
-        (13 * TCO_SIM_HEIGHT)  / 72,
-        (12 * TCO_SIM_HEIGHT)  / 72,
-        (11 * TCO_SIM_HEIGHT)  / 72,
-        (10 * TCO_SIM_HEIGHT)  / 72,
-        (9 * TCO_SIM_HEIGHT)   / 72,
-        (8 * TCO_SIM_HEIGHT)   / 72,
-        (7 * TCO_SIM_HEIGHT)   / 72,
-        (6 * TCO_SIM_HEIGHT)   / 72,
-        (5 * TCO_SIM_HEIGHT)   / 72,
+    const uint16_t target_lines[NUM_VECTOR_POINTS] = {
+        //TODO : Dynamically initialize me.
+        (28 * TCO_FRAME_HEIGHT) / 72, /* Closest to the car */
+        (27 * TCO_FRAME_HEIGHT) / 72,
+        (26 * TCO_FRAME_HEIGHT) / 72,
+        (25 * TCO_FRAME_HEIGHT) / 72,
+        (24 * TCO_FRAME_HEIGHT) / 72,
+        (23 * TCO_FRAME_HEIGHT) / 72,
+        (22 * TCO_FRAME_HEIGHT) / 72,
+        (21 * TCO_FRAME_HEIGHT) / 72,
+        (20 * TCO_FRAME_HEIGHT) / 72,
+        (19 * TCO_FRAME_HEIGHT) / 72,
+        (18 * TCO_FRAME_HEIGHT) / 72,
+        (17 * TCO_FRAME_HEIGHT) / 72,
+        (16 * TCO_FRAME_HEIGHT) / 72,
+        (15 * TCO_FRAME_HEIGHT) / 72,
+        (14 * TCO_FRAME_HEIGHT) / 72,
+        (13 * TCO_FRAME_HEIGHT) / 72,
+        (12 * TCO_FRAME_HEIGHT) / 72,
+        (11 * TCO_FRAME_HEIGHT) / 72,
+        (10 * TCO_FRAME_HEIGHT) / 72,
+        (9 * TCO_FRAME_HEIGHT) / 72,
+        (8 * TCO_FRAME_HEIGHT) / 72,
+        (7 * TCO_FRAME_HEIGHT) / 72,
+        (6 * TCO_FRAME_HEIGHT) / 72,
+        (5 * TCO_FRAME_HEIGHT) / 72,
     };
     uint16_t edges[NUM_VECTOR_POINTS][2];
     /* Find the bottom line */
@@ -75,62 +76,62 @@ void plot_vector_points(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH])
         }
     }
 
-    /* Show the target_lines */
-    #ifdef DRAW
+/* Show the target_lines */
+#ifdef DRAW
     for (int i = 0; i < NUM_VECTOR_POINTS; i++)
         show_target_lines(pixels, target_lines[i]);
-    #endif
+#endif
 
     vector *vectors = calculate_vector(pixels, &target_lines, &edges); /* Calculate where the vector is TODO: Make this return the vector*/
 
-    #ifdef DRAW /* Plot vector points */
+#ifdef DRAW /* Plot vector points */
     for (uint16_t e = 0; e < 2; e++)
     {
-            if (vectors[e].valid == 0)
-                break;
-            plot_line(pixels, vectors[e].bot.x, vectors[e].bot.y, vectors[e].top.x, vectors[e].top.y, 64);
-            plot_square(pixels, vectors[e].bot.x, vectors[e].bot.y, 8, 32);
-            plot_square(pixels, vectors[e].top.x, vectors[e].top.y, 8, 32);
+        if (vectors[e].valid == 0)
+            break;
+        draw_line_horiz(pixels, vectors[e].bot.x, vectors[e].bot.y, vectors[e].top.x, vectors[e].top.y, 64);
+        draw_square(pixels, vectors[e].bot.x, vectors[e].bot.y, 8, 32);
+        draw_square(pixels, vectors[e].top.x, vectors[e].top.y, 8, 32);
     }
-    #endif
+#endif
 
     /* TODO : Don't forget to free the vectors! */
     free(vectors);
 }
 
 /* Utility function to find absolute difference of 2 values */
-uint8_t diff(uint16_t a, uint16_t b) 
+uint8_t diff(uint16_t a, uint16_t b)
 {
     return a < b ? b - a : a - b;
 }
 
-vector *calculate_vector(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], const uint16_t (*target_lines)[NUM_VECTOR_POINTS], const uint16_t (*edges)[NUM_VECTOR_POINTS][2])
+vector *calculate_vector(uint8_t (*pixels)[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH], const uint16_t (*target_lines)[NUM_VECTOR_POINTS], const uint16_t (*edges)[NUM_VECTOR_POINTS][2])
 {
     vector *vectors = calloc(sizeof(vector), 2);
-    for(uint8_t e = 0; e < 2; e++)
+    for (uint8_t e = 0; e < 2; e++)
     {
         uint8_t bot_found = 0; /* Keep track if bottom vector has been found */
-        for(int8_t i = 0; i < NUM_VECTOR_POINTS; i++)
+        for (int8_t i = 0; i < NUM_VECTOR_POINTS; i++)
         {
             if ((*edges)[i][e] == ERR_POINT) /* Skip points that are out of bounds */
                 continue;
 
             if (bot_found == 0) /* Bottom not (yet) found */
             {
-                vectors[e].bot.x = (*edges)[i][e]; /* fill in bot_x */
+                vectors[e].bot.x = (*edges)[i][e];     /* fill in bot_x */
                 vectors[e].bot.y = (*target_lines)[i]; /* fill in bot_y */
                 bot_found = 1;
-            } 
+            }
             else
             {
                 /* If the difference in x is < threshold, accept it. Else, save it */
-                if (diff((*edges)[i-1][e], (*edges)[i][e]) < VECTOR_TOLERANCE) //TODO : Also keep track of the slope (increasing/decreasing). This might let us increase lenience.S
+                if (diff((*edges)[i - 1][e], (*edges)[i][e]) < VECTOR_TOLERANCE) //TODO : Also keep track of the slope (increasing/decreasing). This might let us increase lenience.S
                 {
-                    vectors[e].top.x = (*edges)[i][e]; /* fill in top_x */
+                    vectors[e].top.x = (*edges)[i][e];     /* fill in top_x */
                     vectors[e].top.y = (*target_lines)[i]; /* fill in top_y */
-                    vectors[e].valid = 1; /* WE FOUND A VECTOR! */
+                    vectors[e].valid = 1;                  /* WE FOUND A VECTOR! */
                 }
-                else 
+                else
                 {
                     break; /* This point is not part of the vector! Stop searching */
                 }
--- code/vector.h
+++ code/vector.h
@@ -4,31 +4,30 @@
 #include <stdint.h>
 #include "tco_shmem.h" /* For Image_bounds defintions */
 
-#define DRAW 1 /* Visual representation of points? */
-
 #define NUM_VECTOR_POINTS 24
 #define POINT_TOLERANCE 15
-#define ERR_POINT TCO_SIM_WIDTH + 10 /* The value to set `points` when they are not found */
-#define SEGMENTATION_DEADZONE 20 /* The segmentation we use can sometimes create a white line inbetween horizontal track lines. This is a search offset. */
-#define VECTOR_TOLERANCE 30 /* Number of pixels to cut as 'slack'. The higher the values, the longer the the vectors but less reliable */
+#define ERR_POINT TCO_FRAME_WIDTH + 10 /* The value to set `points` when they are not found */
+#define SEGMENTATION_DEADZONE 20       /* The segmentation we use can sometimes create a white line inbetween horizontal track lines. This is a search offset. */
+#define VECTOR_TOLERANCE 30            /* Number of pixels to cut as 'slack'. The higher the values, the longer the the vectors but less reliable */
 
-typedef struct point_2 {
+typedef struct point_2
+{
     uint16_t x;
     uint16_t y;
 } point;
 
-typedef struct vector_2 {
+typedef struct vector_2
+{
     uint8_t valid;
     point bot;
     point top;
 } vector;
 
-
 /**
  * @brief will perform 5 line scans and plot the points
  * @param pixels A segmented image. See `segmentation.h:segment(...)`
  */
-void plot_vector_points(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH]);
+void plot_vector_points(uint8_t (*pixels)[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH]);
 
 /**
  * @brief will take points and calculate the best suited vector for sides 1 and 2 of `edges`
@@ -36,7 +35,6 @@ void plot_vector_points(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH]);
  * @param edges the list of edges corresponding to `target_lines[i]`. `edges[i][0]` is left, `edges[i][1]` is right.
  * @return A pointer of heap-reserved memory of type vector[2]. 
  */
-vector *calculate_vector(uint8_t (*pixels)[TCO_SIM_HEIGHT][TCO_SIM_WIDTH], const uint16_t (*target_lines)[NUM_VECTOR_POINTS], const uint16_t (*edges)[NUM_VECTOR_POINTS][2]);
-
+vector *calculate_vector(uint8_t (*pixels)[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH], const uint16_t (*target_lines)[NUM_VECTOR_POINTS], const uint16_t (*edges)[NUM_VECTOR_POINTS][2]);
 
 #endif /* _VECTOR_H_ */
